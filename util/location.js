eb2466c240084b1abb163d8cc7531e5cb183d2b0
AzizBoughattas <boughattas.medaziz@esprit.tn>
2023-10-29 20:27:03 +0100

    fix changes

--- util/location.js
+++ util/location.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyC5blgFMJ3_GHFY3RyL8tdtXy3EuqSutGI"; // NO NEED TO USE THE KEY
+const GOOGLE_API_KEY = "Try it with your API KEY";
 
 export function getMapPreview(lat, lng) {
   const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_API_KEY}
